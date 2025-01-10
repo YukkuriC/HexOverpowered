dcb2e37d9093b1dad21c121e2c21ca896ab09319
YukkuriC <799433638@qq.com>
2025-01-10 21:08:17 +0800

    longer modid

--- common/src/main/java/io/yukkuric/hexop/HexOverpowered.java
+++ common/src/main/java/io/yukkuric/hexop/HexOverpowered.java
@@ -3,7 +3,7 @@ package io.yukkuric.hexop;
 import net.minecraft.resources.ResourceLocation;
 
 public abstract class HexOverpowered {
-    public static final String MOD_ID = "hexop";
+    public static final String MOD_ID = "hexoverpowered";
 
     protected static ResourceLocation ID_NEXUS_INVENTORY = new ResourceLocation("hexop:nexus_inv");
     protected static ResourceLocation ID_MEKASUIT_MEDIA_POOL = new ResourceLocation("hexop:mekasuit_media");
