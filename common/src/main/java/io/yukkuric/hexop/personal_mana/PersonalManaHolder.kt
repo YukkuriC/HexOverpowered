ee56ed6915c7c9d84bc8ff5494774a723cc8794d
YukkuriC <799433638@qq.com>
2025-04-03 10:23:36 +0800

    mana loader

---
+++ common/src/main/java/io/yukkuric/hexop/personal_mana/PersonalManaHolder.kt
@@ -0,0 +1,26 @@
+package io.yukkuric.hexop.personal_mana
+
+import at.petrak.hexcasting.api.addldata.ADMediaHolder
+import io.yukkuric.hexop.HexOPAttributes
+import net.minecraft.world.entity.player.Player
+import java.util.WeakHashMap
+
+class PersonalManaHolder(val player: Player) : ADMediaHolder {
+    override fun getMedia() = player.getAttributeBaseValue(HexOPAttributes.PERSONAL_MEDIA).toLong()
+    override fun getMaxMedia() = player.getAttributeBaseValue(HexOPAttributes.PERSONAL_MEDIA_MAX).toLong()
+    override fun setMedia(value: Long) {
+        player.getAttribute(HexOPAttributes.PERSONAL_MEDIA)?.baseValue = value.coerceAtLeast(0).toDouble()
+    }
+
+    override fun canRecharge() = false
+    override fun canProvide() = true
+    override fun getConsumptionPriority() = Int.MAX_VALUE
+    override fun canConstructBattery() = true // try it lol
+
+    companion object {
+        private val MAP = WeakHashMap<Player, PersonalManaHolder>()
+
+        @JvmStatic
+        fun get(player: Player): PersonalManaHolder = MAP.computeIfAbsent(player, ::PersonalManaHolder)
+    }
+}
