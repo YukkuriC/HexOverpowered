8a8f4e517592f4d7ab74d27004bd391c7744c8ac
YukkuriC <799433638@qq.com>
2025-07-12 01:02:45 +0800

    events for personal mana & version++ & update readme

--- common/src/main/java/io/yukkuric/hexop/personal_mana/PersonalManaHolder.kt
+++ common/src/main/java/io/yukkuric/hexop/personal_mana/PersonalManaHolder.kt
@@ -2,14 +2,47 @@ package io.yukkuric.hexop.personal_mana
 
 import at.petrak.hexcasting.api.addldata.ADMediaHolder
 import io.yukkuric.hexop.HexOPAttributes
+import net.minecraft.network.chat.Component
 import net.minecraft.world.entity.player.Player
-import java.util.WeakHashMap
+import java.util.*
 
 class PersonalManaHolder private constructor(val player: Player) : ADMediaHolder {
     override fun getMedia() = player.getAttributeBaseValue(HexOPAttributes.PERSONAL_MEDIA).toInt()
     override fun getMaxMedia() = player.getAttributeValue(HexOPAttributes.PERSONAL_MEDIA_MAX).toInt()
     override fun setMedia(value: Int) {
         player.getAttribute(HexOPAttributes.PERSONAL_MEDIA)?.baseValue = value.coerceAtLeast(0).toDouble()
+    }
+
+    var triggersEvent = true
+    override fun insertMedia(amount: Int, simulate: Boolean): Int {
+        val ret = super.insertMedia(amount, simulate)
+        if (triggersEvent && !simulate) {
+            tryTriggerEvent {
+                PersonalManaEvents.OnInsert(PersonalManaEvents.EventInsert(player, amount, ret))
+            }
+        }
+        return ret
+    }
+
+    override fun withdrawMedia(cost: Int, simulate: Boolean): Int {
+        val ret = super.withdrawMedia(cost, simulate)
+        if (triggersEvent && !simulate) {
+            tryTriggerEvent {
+                PersonalManaEvents.OnExtract(PersonalManaEvents.EventExtract(player, cost, ret))
+            }
+        }
+        return ret
+    }
+
+    private fun tryTriggerEvent(action: Runnable) {
+        triggersEvent = false
+        try {
+            action.run()
+        } catch (e: Throwable) {
+            player.sendSystemMessage(Component.literal("[PersonalMedia] error: $e"))
+            PersonalManaEvents.resetAll()
+        }
+        triggersEvent = true
     }
 
     fun getMediaRegenStep() = player.getAttributeValue(HexOPAttributes.PERSONAL_MEDIA_REGEN).toInt()
