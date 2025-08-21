90a5bb556f42e6da1bed25dc2ab999b2c05cabe5
YukkuriC <799433638@qq.com>
2025-08-22 01:14:04 +0800

    backport path change

--- common/src/main/java/io/yukkuric/hexop/mixin/SuperPlayerIota.java
+++ common/src/main/java/io/yukkuric/hexop/mixin/SuperPlayerIota.java
@@ -1,7 +1,7 @@
 package io.yukkuric.hexop.mixin;
 
-import at.petrak.hexcasting.api.casting.iota.EntityIota;
-import at.petrak.hexcasting.api.casting.iota.IotaType;
+import at.petrak.hexcasting.api.spell.iota.EntityIota;
+import at.petrak.hexcasting.api.spell.iota.IotaType;
 import com.llamalad7.mixinextras.sugar.Local;
 import io.yukkuric.hexop.HexOPConfig;
 import net.minecraft.nbt.NbtUtils;
@@ -12,9 +12,9 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(targets = {"at.petrak.hexcasting.api.casting.iota.EntityIota$1"})
+@Mixin(targets = {"at.petrak.hexcasting.api.spell.iota.EntityIota$1"})
 public abstract class SuperPlayerIota extends IotaType<EntityIota> {
-    @Inject(method = "deserialize(Lnet/minecraft/nbt/Tag;Lnet/minecraft/server/level/ServerLevel;)Lat/petrak/hexcasting/api/casting/iota/EntityIota;", at = @At("RETURN"), cancellable = true)
+    @Inject(method = "deserialize(Lnet/minecraft/nbt/Tag;Lnet/minecraft/server/level/ServerLevel;)Lat/petrak/hexcasting/api/spell/iota/EntityIota;", at = @At("RETURN"), cancellable = true)
     void hook(Tag tag, ServerLevel world, CallbackInfoReturnable<EntityIota> cir, @Local(ordinal = 1) Tag uuidTag) {
         if (uuidTag == null || !HexOPConfig.TrueNameCrossDimension() || cir.getReturnValue() != null) return;
         var players = world.getServer().getPlayerList();
