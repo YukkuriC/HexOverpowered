2f989450326fe30f62893509bda0d4d125c20d0b
YukkuriC <799433638@qq.com>
2026-02-13 13:32:43 +0800

    fix inner exception -> mishap

--- common/src/main/java/io/yukkuric/hexop/mixin/hexcellular/ExecutableProperty.java
+++ common/src/main/java/io/yukkuric/hexop/mixin/hexcellular/ExecutableProperty.java
@@ -3,8 +3,10 @@ package io.yukkuric.hexop.mixin.hexcellular;
 import at.petrak.hexcasting.api.casting.SpellList;
 import at.petrak.hexcasting.api.casting.eval.CastResult;
 import at.petrak.hexcasting.api.casting.eval.ResolvedPatternType;
+import at.petrak.hexcasting.api.casting.eval.sideeffects.OperatorSideEffect;
 import at.petrak.hexcasting.api.casting.eval.vm.*;
 import at.petrak.hexcasting.api.casting.iota.*;
+import at.petrak.hexcasting.api.casting.mishaps.Mishap;
 import at.petrak.hexcasting.api.casting.mishaps.MishapEvalTooMuch;
 import at.petrak.hexcasting.common.lib.hex.HexEvalSounds;
 import io.yukkuric.hexop.HexOPConfig;
@@ -39,7 +41,10 @@ public abstract class ExecutableProperty extends Iota {
 
         // add to cont.
         var newCont = continuation.pushFrame(FrameFinishEval.INSTANCE).pushFrame(new FrameEvaluate(spell, true));
-        if (vm.getImage().getOpsConsumed() > vm.getEnv().maxOpCount()) throw new MishapEvalTooMuch();
+        if (vm.getImage().getOpsConsumed() > vm.getEnv().maxOpCount())
+            return new CastResult(this, SpellContinuation.Done.INSTANCE, vm.getImage(), List.of(
+                    new OperatorSideEffect.DoMishap(new MishapEvalTooMuch(), new Mishap.Context(null, null))
+            ), ResolvedPatternType.ERRORED, HexEvalSounds.MISHAP);
 
         // return
         return new CastResult(this, newCont, vm.getImage().withUsedOp(), List.of(), ResolvedPatternType.EVALUATED, HexEvalSounds.NOTHING);
