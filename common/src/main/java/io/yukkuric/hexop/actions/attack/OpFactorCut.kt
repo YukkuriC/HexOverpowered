d9a71e78fd2b60177f800724d053643330a5b40d
YukkuriC <799433638@qq.com>
2026-05-25 00:07:00 +0800

    factor cut truename protection

--- common/src/main/java/io/yukkuric/hexop/actions/attack/OpFactorCut.kt
+++ common/src/main/java/io/yukkuric/hexop/actions/attack/OpFactorCut.kt
@@ -11,15 +11,13 @@ import at.petrak.hexcasting.api.casting.getInt
 import at.petrak.hexcasting.api.casting.iota.DoubleIota
 import at.petrak.hexcasting.api.casting.iota.EntityIota
 import at.petrak.hexcasting.api.casting.iota.Iota
-import at.petrak.hexcasting.api.casting.mishaps.MishapDisallowedSpell
-import at.petrak.hexcasting.api.casting.mishaps.MishapInvalidIota
-import at.petrak.hexcasting.api.casting.mishaps.MishapNotEnoughArgs
-import at.petrak.hexcasting.api.casting.mishaps.MishapNotEnoughMedia
+import at.petrak.hexcasting.api.casting.mishaps.*
 import at.petrak.hexcasting.api.pigment.FrozenPigment
 import io.yukkuric.hexop.HexOPConfig
 import io.yukkuric.hexop.helpers.GetPigment
 import io.yukkuric.hexop.helpers.PrimeChecker
 import io.yukkuric.hexop.helpers.attack.EntityHealthAccessors
+import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.DyeColor
 import net.minecraft.world.phys.Vec3
 import kotlin.math.acos
@@ -58,6 +56,9 @@ object OpFactorCut : ConstMediaAction {
         if (args.size == 1) {
             return listOf(DoubleIota(healthAsInt.toDouble()))
         }
+
+        // config to deny cutting for true-names
+        if (HexOPConfig.FactorCutPlayerProtection() && target is Player) throw MishapOthersName(target)
 
         // health cut
         val factor = args.getInt(1, args.size)
